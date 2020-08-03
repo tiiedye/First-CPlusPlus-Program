59b8b143ae8f3780af4edafc8774b618e9753e98
tiiedye <tiiedye@gmail.com>
2020-08-03 12:54:01 -0700

    finishing up smiple response

--- FirstProgram/main.cpp
+++ FirstProgram/main.cpp
@@ -5,7 +5,9 @@ int main() {
 	
 	std::cout << "Enter your favorite number between 1 and 100: ";
 	std::cin >> favorite_number;
-	std::cout << "Amazing, that's my favorite number too!" << std::endl;
+	std::cout << "Amazing, that's my favorite number, too!" << std:: endl;
 	
-	return 0;
+	std::cout << "No, really!! " << favorite_number << " is my favorite number!!" << std:: endl;
+	
+		return 0;
 }
