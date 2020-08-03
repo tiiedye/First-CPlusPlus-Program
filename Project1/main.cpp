c314ff0d1f7b78fe0dee0c659c7c7340675e6b99
tiiedye <tiiedye@gmail.com>
2020-08-03 11:20:23 -0700

    transfering files

---
+++ Project1/main.cpp
@@ -0,0 +1,6 @@
+#include <iosstream>
+
+int main(int argc, char **argv) {
+	printf("hello Project 1");
+	return 0;
+}
---
+++ Project2/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int argc, char **argv) {
+	printf("hello project 2");
+	return 0;
+}
